ee13598b8cad6ef2137cbb949b24b79eedcc9561
Omar Cuellar Barrero <omarcuellarbarrero@gmail.com>
2026-05-23 08:35:52 -0500

    Update index.js

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.use((req, res, next) => {
 });
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, 'buscador.html'));
 });
 
 app.post('/api/diagnostico', async (req, res) => {
