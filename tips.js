4ded16d56bf73f021a5c784f1a785209fdc59c1f
Omar Cuellar Barrero <omarcuellarbarrero@gmail.com>
2026-05-23 21:47:16 -0500

    Update tips.js

--- tips.js
+++ tips.js
@@ -8,5 +8,3 @@ MARCA: RCA | MODELO: HK.T.RT2831P637 | SINTOMA: Pegado en Logo | SOLUCION: [SOFT
 MARCA: LG | MODELO: 32LB550B | SINTOMA: Sonido OK, sin imagen | SOLUCION: Cambio de regletas LED. Verificar V-out > 72V.
 MARCA: SONY | MODELO: KDL-32M3000 | SINTOMA: Imagen lenta/rojiza | SOLUCION: Jumper VGH desde T-CON a COF lateral.
 `;
-
-export { tipsContenido };
